c839574d8f9a2ac7fe451a66b156a17d83069b83
Joshua Coon <jjc16_1@yahoo.com>
2023-04-15 16:58:27 -0400

    automated commit

--- build_prod.sh
+++ build_prod.sh
@@ -2,8 +2,8 @@ JEKYLL_ENV=production bundle exec jekyll build
 #cd ./_site
 cp -v _site/*.* jjc16.github.io/
 cp -rv assets/ jjc16.github.io/
-cp -rv _posts/*.* jjc16.github.io/_posts/
-cp -rv _posts/*.* jjc16.github.io/
+cp -rv posts/*.* jjc16.github.io/_posts/
+cp -rv posts/*.* jjc16.github.io/
 cd jjc16.github.io
 git add .
 git commit -am"automated commit"
